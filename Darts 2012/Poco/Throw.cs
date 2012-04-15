2780b360be450776328675175f586e33ff8613ec
McNeil <mcneil@gmx.net>
2012-04-15 23:08:37 +0200

    first part of DoubleOutChart

--- Darts 2012/Poco/Throw.cs
+++ Darts 2012/Poco/Throw.cs
@@ -4,5 +4,33 @@ namespace Darts_2012.Poco
     {
         public int Value { get; set; }
         public int Multiplier { get; set; }
+
+        public override string ToString()
+        {
+            var throwString = "";
+            if (Value == 25)
+            {
+                if (Multiplier == 2)
+                {
+                    throwString += "D";
+                }
+                throwString += "B";
+
+                return throwString;
+            }
+            
+            if (Multiplier == 2)
+            {
+                throwString += "D";
+            }
+            else if (Multiplier == 3)
+            {
+                throwString += "T";
+            }
+
+            throwString += Value.ToString();
+
+            return throwString;
+        }
     }
 }
